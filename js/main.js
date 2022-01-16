59e989c4fa2b29910cd036e1bfe23811e7cad2e7
joshuacba08 <joshuacba08@gmail.com>
2022-01-16 20:02:16 -0300

    calibración de agujas

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 /*varibales*/
 const seconds = document.querySelectorAll('.seconds');
-const minute = document.querySelectorAll('.minute');
-
+const minutes = document.querySelectorAll('.minute');
+const hours = document.querySelectorAll('.hour');
 
 /*funciones*/
 const setSeconds = () => {
 
     seconds.forEach( element => {
-        element.style.setProperty('--initial', '0deg');
-        element.style.setProperty('--final','360deg');
-        // element.style.transform = 'rotate(300deg)';
+        element.style.setProperty('--initial', '160deg');
+        element.style.setProperty('--final','520deg');
+        element.style.animation = `clocking 60s infinite linear`;
+    });
+}
+
+const setMinutes = () => {
+    minutes.forEach( element => {
+        element.style.setProperty('--initial', '300deg');
+        element.style.setProperty('--final','660deg');
         element.style.animation = `clocking 3600s infinite linear`;
     });
+}
 
+const setHours = () => {
+    hours.forEach( element => {
+        element.style.setProperty('--initial', '30deg');
+        element.style.setProperty('--final','390deg');
+        element.style.animation = `clocking  43200s infinite linear`;
+    });
 }
 
 /*eventos*/
@@ -20,14 +34,15 @@ const setSeconds = () => {
 window.addEventListener('DOMContentLoaded', () => {
 
     setSeconds();
+    setMinutes();
+    setHours();
+        // minute.forEach( element => {
 
-    minute.forEach( element => {
-
-        element.style.setProperty('--initial', '300deg');
-        element.style.setProperty('--final','299deg');
-        // element.style.transform = 'rotate(300deg)';
-        element.style.animation = `clocking 3600s infinite linear`;
-    });
+        //     element.style.setProperty('--initial', '300deg');
+        //     element.style.setProperty('--final','299deg');
+        //     // element.style.transform = 'rotate(300deg)';
+        //     element.style.animation = `clocking 3600s infinite linear`;
+        // });
 
 });
 
